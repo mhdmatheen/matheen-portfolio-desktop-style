b6325b6fe3e991a489dad01fff805c4e2074fb6d
matheen <mhdmatheen@gmail.com>
2025-07-03 21:24:21 +0530

    updated windows list

--- src/config/window-list.tsx
+++ src/config/window-list.tsx
@@ -1,11 +1,11 @@
 export const windowItems = [
-    { name: "Resume", icon: <img className="w-8 h-8" src="/cv.png" alt="about me window" draggable={false} />, app: 'Resume' },
-    { name: "About Me", icon: <img className="w-8 h-8" src="/programmer.png" alt="about me window" draggable={false} />, app: 'About' },
-    { name: "My Experiences", icon: <img className="w-8 h-8" src="/suitcase.png" alt="experiences window" draggable={false} />, app: 'Experiences' },
-    { name: "My Projects", icon: <img className="w-8 h-8" src="/folders.png" alt="projects window" draggable={false} />, app: 'Projects' },
-    { name: "My Skills", icon: <img className="w-8 h-8" src="/soft-skills.png" alt="skills window" draggable={false} />, app: 'Skills' },
-    { name: "About this Project", icon: <img className="w-8 h-8" src="/binary-code.png" alt="code window" draggable={false} />, app: 'Code' },
-    { name: "Feedback", icon: <img className="w-8 h-8" src="/chat.png" alt="feedback window" draggable={false} />, app: 'Feedback' },
+    { name: "Resume", icon: <img className="w-8 h-8" src="/cv.png" alt="about me window" draggable={false} />, app: 'Resume', type: 'window' },
+    { name: "About Me", icon: <img className="w-8 h-8" src="/programmer.png" alt="about me window" draggable={false} />, app: 'About', type: 'window' },
+    { name: "My Experiences", icon: <img className="w-8 h-8" src="/suitcase.png" alt="experiences window" draggable={false} />, app: 'Experiences', type: 'window' },
+    { name: "My Projects", icon: <img className="w-8 h-8" src="/folders.png" alt="projects window" draggable={false} />, app: 'Projects', type: 'window' },
+    { name: "My Skills", icon: <img className="w-8 h-8" src="/soft-skills.png" alt="skills window" draggable={false} />, app: 'Skills', type: 'window' },
+    { name: "About this Project", icon: <img className="w-8 h-8" src="/binary-code.png" alt="code window" draggable={false} />, app: 'Code', type: 'window' },
+    { name: "Feedback", icon: <img className="w-8 h-8" src="/chat.png" alt="feedback window" draggable={false} />, app: 'Feedback', type: 'window' },
     { name: "Minesweeper", 
         icon: <img className="w-8 h-8" src="/minesweeper.png" alt="code window" draggable={false} />, 
         app: 'Minesweeper',
@@ -34,8 +34,8 @@ export const windowItems = [
     },
 ];
 
-export const startMenuItems = windowItems.filter(item => item.app !== 'Email');
+export const startMenuItems = windowItems.filter(item => item.type === 'window');
 
 export const desktopIcons = windowItems.filter(item => item.app === 'Resume' || item.app === 'About' || item.app === 'Experiences' || item.app === 'Projects' || item.app === 'Skills');
 
-export const secondaryMenuItems = windowItems.filter(item => item.app === 'Minesweeper' || item.app === 'Email' || item.app === 'Linkedin' || item.app === 'ZorinBytes');
+export const secondaryMenuItems = windowItems.filter(item => item.type === 'app' || item.type === 'link');
